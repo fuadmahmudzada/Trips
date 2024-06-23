31e0985446dd73e28210fc30f66d6490ee4304d5
fuadmahmudzada <157024891+fuadmahmudzada@users.noreply.github.com>
2024-06-23 08:32:24 +0400

    Revert "Feature/email sending"

--- src/main/java/com/trips/mvc/controllers/HomeController.java
+++ src/main/java/com/trips/mvc/controllers/HomeController.java
@@ -1,7 +1,6 @@
 package com.trips.mvc.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -22,7 +21,7 @@ public class HomeController {
 //    }
 
     @GetMapping("/contact")
-    public String contact(Model model) {
+    public String contact() {
         return "contact";
     }
 
